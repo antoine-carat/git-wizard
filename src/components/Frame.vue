c1545caadd2b14972aca3d327cea1756808cea7d
Antoine Carat <45177824+antoine-carat@users.noreply.github.com>
2019-10-23 18:39:37 +0100

    Build up main frame (#2)
    * Replace HelloWorld by Frame + remove logo + rename page
    
    * Move title out of frame
    
    * Add frame design
    
    * Add item design
    
    * Add items default examples

---
+++ src/components/Frame.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="frame">
+    <span class="item" v-for="item in items" :key="item.title">
+      <span class="title">{{ item.title }}</span>
+      <input type="checkbox" class="box" name="" id="" :checked="item.done">
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Frame",
+  props: {
+    items: {
+      type: Array,
+      default: () => [ { title: "Call Bob", done: false },
+                       { title: "Pick up Alice at school", done: true } ]
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.frame {
+  display: flex;
+  flex-flow: column wrap;
+  align-content: center;
+  justify-content: space-evenly;
+
+  border: 1px dashed #2c3e50;
+  padding: 1%;
+  width: 80%;
+
+  .item {
+    display: flex;
+    flex-flow: row wrap;
+    align-content: center;
+    justify-content: space-between;
+    text-align: left;
+
+    border: 1px solid #2c3e50;
+    padding: 1%;
+    margin: 1% 0%;
+    width: 98%;
+  }
+}
+</style>
